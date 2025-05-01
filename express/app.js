645592751d84cc966eacd7e70ae7b1acc00d3ab2
hussienfci <hussienfci@gmail.com>
2025-05-01 20:47:43 +0200

    CRUD commands in app.js

--- express/app.js
+++ express/app.js
@@ -19,14 +19,49 @@ app.get('/users' , (req , res)=>{
     res.json(data) ; 
 }) ;
 
-app.get('/user/:id' , (req, res)=> {
-    const userId = req.params.id 
+app.get('/user/:userName' , (req, res)=> {
+    const userId = req.params.username 
     const obj = {name:req.query.name , age:req.query.age} ; 
     console.log(obj);
     const objStr = JSON.stringify(obj) ;
-    const data = {message:`Hello from API User ID:  ${userId} ${}` , objStr} ;
+    const data = {message:`Hello from API User ID:  ${userId} ` , objStr} ;
     res.json(data) ;
 }); 
+
+
+app.post('/user',(req, res) =>{
+    const userData = req.body ; 
+    console.log(userData);
+    const data = {message: "User created successfully" , user:userData} ; 
+    res.json(data) ;
+    
+})
+
+
+// full object
+app.put('/user/:age' , (req , res)=>{
+    // const age = req.query.age ; 
+    
+    const age = req.params.age ; 
+    const userData = req.body ; 
+    const data = {message:`User age ${age} updated successfully` , user:userData} 
+    res.json(data) ;
+}) ; 
+
+// partially object
+app.patch('/user/:id', (req , res) =>{
+    const userId = req.params.id ; 
+    const userData = req.body ; 
+    const data = {message: `User Id ${userId} partially updated successfully!` , user:userData} ; 
+    res.json(data) ; 
+
+})
+
+app.delete('/user/:username' , (req, res)=>{
+    const userName = req.params.username ; 
+    const data = {message: `User-name: ${userName} has been deleted successfully :)` } ; 
+    res.json(data) 
+})
 
 app.listen(5000 , () => {
     console.log('server is running on port 3000');
